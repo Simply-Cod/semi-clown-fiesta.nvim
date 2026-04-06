af7e6eb9c69375e00b8d75c20e3448f9012b0ab2
Morten <simply.cod.92@gmail.com>
2026-04-06 13:34:31 +0200

    Added mini.files

--- lua/no-clown-fiesta/groups/mini.lua
+++ lua/no-clown-fiesta/groups/mini.lua
@@ -1,12 +1,24 @@
 local M = {}
 
 function M.highlight(palette, opts)
-  return {
-    MiniStatuslineFilename = { fg = palette.light_gray, bg = palette.bg },
-    MiniStatuslineDevinfo = { fg = palette.medium_gray, bg = palette.bg },
-    MiniStatuslineFileinfo = { fg = palette.medium_gray, bg = palette.bg },
-    MiniStatuslineInactive = { fg = palette.gray, bg = palette.bg },
-  }
+    return {
+
+        -- mini.statusline
+        MiniStatuslineFilename = { fg = palette.light_gray, bg = palette.bg },
+        MiniStatuslineDevinfo = { fg = palette.medium_gray, bg = palette.bg },
+        MiniStatuslineFileinfo = { fg = palette.medium_gray, bg = palette.bg },
+        MiniStatuslineInactive = { fg = palette.gray, bg = palette.bg },
+
+        -- mini.files
+        MiniFilesBorder = { fg = palette.fg },
+        MiniFilesBorderModified = { bg = palette.sign_change },
+        MiniFilesCursorLine = { bg = palette.alt_bg },
+        MiniFilesDirectory = { fg = palette.blue },
+        MiniFilesFile = { fg = palette.fg },
+        MiniFilesNormal = { fg = palette.fg, bg = palette.bg },
+        MiniFilesTitle = { fg = palette.fg },
+        MiniFilesTitleFocused = { fg = palette.cyan },
+    }
 end
 
 return M
